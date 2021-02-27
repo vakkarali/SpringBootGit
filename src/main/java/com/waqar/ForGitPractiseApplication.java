b98bcf8c406fd2ecb69f423aabfd880863ed20b1
vakkarali <vakkarali2011@gmail.com>
2021-02-27 23:25:49 +0530

    for spring boot project

---
+++ src/main/java/com/waqar/ForGitPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.waqar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGitPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForGitPractiseApplication.class, args);
+	}
+
+}
